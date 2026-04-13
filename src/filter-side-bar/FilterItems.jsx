ad5e423777b1d399dc4d748281390dd030b87045
Yaohan Li <yaohanli@Yaohans-MacBook-Air.local>
2026-04-13 16:13:16 +0200

    add-filter-items

--- src/filter-side-bar/FilterItems.jsx
+++ src/filter-side-bar/FilterItems.jsx
@@ -1,9 +1,19 @@
-export default function FilterItems () {
-    return <div>
-        <ul>
-            <li>Lastest arrivals</li>
-            <li>Urban Oasis</li>
-            <li>Cozy Comfort</li>            
-        </ul>
-    </div>
+export default function FilterItems ({options}) {
+    const filterItems = options.map((option, index) => {
+        const proteryName=option.toLowerCase().replace(" ", "-");
+                
+        return ( 
+            <label className="filter-option" key={index}> 
+                <input type="checkbox" name={proteryName} />
+                {option}
+            </label>                  
+
+        )
+    });
+
+    return (
+        <>
+            {filterItems}
+        </>
+    )
 }
--- src/filter-side-bar/FilterSideBar.jsx
+++ src/filter-side-bar/FilterSideBar.jsx
@@ -1,10 +1,50 @@
-import FilterSection from "./FilterSection"
+import './FilterSideBar.css';
+import FilterItems from './FilterItems';
+import { useState } from 'react';
+
+
+function FilterSection({sectionName, options}) {
+    const [isClick, setIsClick] = useState(false);    
+
+    return (
+        <div className='filter-section'>
+            <div className="filter-head" onClick={() => setIsClick(!isClick)}>
+                <p>{sectionName}</p>
+                <p> {isClick?"-":"+"} </p>            
+            </div>
+            <div className="filter-items" hidden={isClick}>
+                <FilterItems options={options} />
+            </div>
+            
+            <hr />
+        </div>
+    )
+}
 
 export default function FilterSideBar() {
-    return <div>
-        <FilterSection sectionName="Collections"/>
-        <FilterSection sectionName="Category"/>
-        <FilterSection sectionName="Color"/>
-        <FilterSection sectionName="Rating"/>
-    </div>
+    const filterSection = filters.map(filter =>  <FilterSection key={filter.sectionName} sectionName={filter.sectionName} options={filter.options} />);
+    return (
+        <div>
+             {filterSection}
+        </div>
+    )
 }
+
+const filters = [
+    {
+        sectionName: "Collections" ,
+        options: ["Lastest arrivals", "Urban Oasis", "Cozy Comfort", "Fresh Fusion"],
+    },
+    {
+        sectionName: "Category",
+        options: [ "Men", "Women", "Unisex",],
+    },
+    {
+        sectionName: "Color",
+        options: ["blue","brown", "beige", "white", "red", "orange", "brown","black", "green"],
+    },
+    {
+        sectionName: "Rating",
+        options: [],
+    }        
+];
