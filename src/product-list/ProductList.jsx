c068d3341a75e77006ebe906e64c8c2de750abff
Yaohan Li <yaohanli@Yaohans-MacBook-Air.local>
2026-04-16 22:00:32 +0200

    feat: implement the drop-down menu

--- src/product-list/ProductList.jsx
+++ src/product-list/ProductList.jsx
@@ -3,7 +3,8 @@ import ProductCard from "./ProductCard";
 import { useState, useEffect } from "react";
 
 export default function ProductList({isFilter}) {
-    const [products, setProducts] = useState();   
+    const [products, setProducts] = useState();
+    const [isSort, setSort] = useState();
 
     async function pullJson() {
         let productUrl = new URL("https://www.greatfrontend.com/api/projects/challenges/e-commerce/products");
@@ -16,7 +17,22 @@ export default function ProductList({isFilter}) {
                 productUrl.searchParams.append(key.toLowerCase(), value);
             })
         } 
-        
+
+        // isSort => Apply Sorting
+        if (isSort) {
+            let sortPar;
+            if (isSort.slice(0,5) === "price") {
+                sortPar = "price";
+                const directionPar = isSort.slice(6);
+                productUrl.searchParams.append("direction", directionPar);                
+            } else {
+                sortPar = isSort;
+            }
+
+            productUrl.searchParams.append("sort", sortPar);            
+        }
+
+        // Get product data from API and Render them
         let response = await fetch (productUrl);          
         let result = await response.json();           
         let resultData = Array.from(result.data);            
@@ -34,11 +50,11 @@ export default function ProductList({isFilter}) {
 
     useEffect(() => {
         pullJson();              
-    }, [isFilter]);
+    }, [isFilter, isSort]);
 
     return(
         <div >
-            <DropDownMenu />
+            <DropDownMenu setSort={setSort}/>
             <div className="product-list">
                 {products}
             </div>
