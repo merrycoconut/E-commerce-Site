703514f709d8862c06312b3d237c0825e2fec170
Yaohan Li <pandoralyh1228@gmail.com>
2026-06-12 16:15:43 +0200

    fix & refactor: 1. fix some minor bugs 2. rewrite the README

--- src/components/filter-side-bar/FilterSideBar.jsx
+++ src/components/filter-side-bar/FilterSideBar.jsx
@@ -3,17 +3,15 @@ import FilterSection from "./FilterSection.jsx";
 import { productFilters } from "../../utils/productFilterData.jsx";
 
 export default function FilterSideBar({ applyFilter, clearFilter }) {
-  const filterSection = productFilters.map((filter) => (
-    <FilterSection
-      key={filter.sectionName}
-      sectionName={filter.sectionName}
-      options={filter.options}
-    />
-  ));
-
   return (
     <form action={(formData) => applyFilter(formData)}>
-      {filterSection}
+      {productFilters.map((filter) => (
+        <FilterSection
+          key={filter.sectionName}
+          sectionName={filter.sectionName}
+          options={filter.options}
+        />
+      ))}
       <div className="filter-button-container">
         <button type="submit">Apply</button>
         <button onClick={() => clearFilter()}>Clear All</button>
